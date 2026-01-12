5368edcbeff8cfab7de35f1a105a916f3b70adde
Mayank Rajput <103987586+mayanksinghrajput1212@users.noreply.github.com>
2026-01-12 15:56:14 +0530

    Create build.gradle.kts with Android and Kotlin plugin definitions

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("com.android.application") version "8.1.0"
+    kotlin("android") version "1.9.0"
+    kotlin("kapt") version "1.9.0"
+}
+
+android {
+    namespace = "com.example.projectfachlist"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.projectfachlist"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    // AndroidX
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+
+    // Testing
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
